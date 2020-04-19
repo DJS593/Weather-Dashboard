a7faeecd7a861e0bf3b384800a2b2e0e63c8360c
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-04-18 22:14:30 -0700

    added seperate event listener calls for each function and adjusted some of the html

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,8 @@ var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#city");
 var weatherContainerEl = document.querySelector("#weather-container");
 var priorCityEl = document.querySelector("#past-city");
+var buttonEl = document.querySelector("#btn-search");
 
-//var citySearch = cityInputEl.textContent;
-//var citySearch =("novato");
-// console.log(citySearch);
-
-
-// console.log(userFormEl);
-// console.log(cityInputEl);
-// console.log(weatherContainerEl);
-// console.log(priorCity);
 
 
 
@@ -24,8 +16,7 @@ var priorCityEl = document.querySelector("#past-city");
 // var apiUrlForecast = "https://api.openweathermap.org/data/2.5/forecast?q=novato&units=imperial&appid=9aa19330ef7b3b00f2721d639d19782d";
 
 
-// var citySearch = ("novato");
-// var apiUrlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&units=imperial&appid=9aa19330ef7b3b00f2721d639d19782d";
+
 
 
 
@@ -35,21 +26,13 @@ var priorCityEl = document.querySelector("#past-city");
 
 
 
-// need to ask how to make the fetch call part of a function - not sure what I am doing wrong
-// var getWeather = function() {
 
-// create a long / lat var to call the UV
-
-//var currentWeather = function () {
-
-function myFunction() {
+var currentWeather = function(event) {
+// function myFunction() {
 
   // make the city dynamic in the API call
   var citySearch = document.getElementById("city").value;
-  //var cityLongitude = data.coord.lon; 
-  //var cityLatitude = data.coord.lat;
-  //console.log(cityLongitude, cityLatitude);
-
+  
   var apiUrlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&units=imperial&appid=9aa19330ef7b3b00f2721d639d19782d";
 
   fetch(apiUrlCurrent).then(function(response) {
@@ -71,92 +54,96 @@ function myFunction() {
     
 
     // current date
-    var currDate = document.createElement("h4");
-    currDate.classList.add("current-date");
+    var currDate = document.querySelector("#currDate");
     currDate.textContent = ("("+moment().format('l')+")");
     currContainerEl.appendChild(currDate);
 
     // current weather icon  // need to add URL for png file
     // http://openweathermap.org/img/wn/10d@2x.png  replace code "10d" with current code
-    var currIcon = document.createElement("span");
-    currIcon.classList.add("current-icon");
-    currIcon.textContent = ("http://openweathermap.org/img/wn/"+data.weather.icon+"2x.png");
+    var currIcon = document.querySelector("#currIcon");
+    urlCurrIcon = ("http://openweathermap.org/img/wn/"+data.weather.icon+"2x.png");
+    currIcon.setAttribute("src", urlCurrIcon);
+    //currIcon.textContent = ("http://openweathermap.org/img/wn/"+data.weather.icon+"2x.png");
     currContainerEl.appendChild(currIcon);
     
     // current temp
-    var currTemp = document.createElement("p");
-    currTemp.classList.add("current-temp");
-    roundCurrTemp = 
+    var currTemp = document.querySelector("#currTemp");
     currTemp.textContent = "Temperature: " + Math.round(data.main.temp) + " F";
     currContainerEl.appendChild(currTemp);
-    //console.log("temp", data.main.temp);
 
     // current humidity
-    var currHumidity = document.createElement("p");
-    currHumidity.classList.add("current-humid");
+    var currHumidity = document.querySelector("#currHumidity");
     currHumidity.textContent = "Humidity: " + data.main.humidity + "%";
     currContainerEl.appendChild(currHumidity);
-    //console.log("humidity", data.main.humidity);
 
     // current wind speed
-    var currWind = document.createElement("p");
-    currWind.classList.add("current-wind");
+    var currWind = document.querySelector("#currWind");
     currWind.textContent = "Wind Speed: " + data.wind.speed + " MPH";
     currContainerEl.appendChild(currWind);
-    //console.log("wind", data.wind.speed);
 
-      // current UV index
-      // requires geocode
+    
+    // current UV index
+    // requires geocode
+ 
+    
     var cityLongitude = data.coord.lon; 
     var cityLatitude = data.coord.lat;
     console.log(cityLongitude, cityLatitude);
     var apiUrlUv = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLatitude + "&lon=" + cityLongitude + "&appid=9aa19330ef7b3b00f2721d639d19782d";
     fetch(apiUrlCurrent).then(function(response) {
       response.json().then(function(data) {
-        console.log(data);
+        //console.log(data);
     
-      var currUv = document.createElement("p");
-      currUv.classList.add("current-uv");
-      currUv.textContent = "UV Index: this is a placeholder";
+      var currUv = document.querySelector("#currUv");
+      //currUv.classList.add("current-uv");
+      currUv.textContent = "UV Index: " + data.value;
       currContainerEl.appendChild(currUv);
-      console.log("placeholder")
+      console.log("currUv")
 
       })})
     
-   })
+    })
     
- });
+  });
+     
+ };
     
-};
-
-
-
-//};
-
-
-// var futureWeather = function () {}
+ 
+    
 
 
 
 
-// fetch(apiUrlForecast).then(function(response) {
-//   response.json().then(function(data) {
-//     // displayWeather(data);
-//     console.log(data);
-//     var forecastTemp = data.list[0].main.temp;
-//     console.log(forecastTemp);
+
+
+var forecastWeather = function (event) {
+
+var citySearch = document.getElementById("city").value;  
+var apiUrlForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&units=imperial&appid=9aa19330ef7b3b00f2721d639d19782d";
+
+fetch(apiUrlForecast).then(function(response) {
+  response.json().then(function(data) {
+    console.log(data);
+    //var forecastTemp = data.list[0].main.temp;
+    // console.log(forecastTemp);
    
-//     // var forecastHumidity = data.list.humidity;
-//     // var forecastWind = data.list.wind;
-//     // var forecastUv = data.list.uv;
-//     // console.log(apiTemp);
-//     // varTempEl = document.createElement("p");
-//      // varTempEl.setAttribute(); // need to add here if I use //
-//       //document.body.appendChild(varTempEl); // not setting to the body, but placeholder for now //
-//     });
-//   });
-// // };
-  
+    // var forecastHumidity = data.list.humidity;
+    // var forecastWind = data.list.wind;
+    // var forecastUv = data.list.uv;
+    // console.log(apiTemp);
+    // varTempEl = document.createElement("p");
+     // varTempEl.setAttribute(); // need to add here if I use //
+      //document.body.appendChild(varTempEl); // not setting to the body, but placeholder for now //
+    });
+  });
+
+}
+
+
+
+
+
+
 
 
 // local storage - need to keep the city names and data persistent
@@ -179,16 +166,10 @@ $("#past-city").val(JSON.parse(localStorage.getItem("city")));
 // append the data to the page
 
 
-// current time and city
 
-  // time we can modify the below moment() and append
-  // var rightNow = moment().format("dddd MMMM Do");
-  // $("#currentDay").append(rightNow);
-  // console.log(rightNow);
 
-// Might use ths function for displaying the current conditions
+// addEventListener for the button click to begin functions
 
- displayWeather = function(data, city) {
-
-}
+buttonEl.addEventListener("click", currentWeather);
+buttonEl.addEventListener("click", forecastWeather);
 
